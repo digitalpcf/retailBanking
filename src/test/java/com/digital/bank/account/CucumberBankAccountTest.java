b41b940a7222c05ffab2ced11595f7eab7167bcb
Amit <amit.datta2@wipro.com>
2018-11-13 12:23:16 +0530

    New Scenario and validation added.

--- src/test/java/com/digital/bank/account/CucumberBankAccountTest.java
+++ src/test/java/com/digital/bank/account/CucumberBankAccountTest.java
@@ -4,8 +4,10 @@ package com.digital.bank.account;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(Cucumber.class)
+@ActiveProfiles
 @CucumberOptions(features = "src/main/resources/feature", glue="com.digital.bank.account.stepdef")
 public class CucumberBankAccountTest {
 }
