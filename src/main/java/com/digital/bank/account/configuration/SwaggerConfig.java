9ff19e1eaef0c350f4b93fa235d66d0592fb686e
Amit <amit.datta2@wipro.com>
2018-11-16 18:48:38 +0530

    Endpoint removed.

---
+++ src/main/java/com/digital/bank/account/configuration/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.digital.bank.account.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/* To check api use the url:
+
+    http://localhost:8002/v2/api-docs
+    and
+    http://localhost:8002/swagger-ui.html
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()                 .apis(RequestHandlerSelectors.basePackage("com.digital.bank.account.controller"))
+                .paths(regex("/retailbanking.*"))
+                .build();
+    }
+}
