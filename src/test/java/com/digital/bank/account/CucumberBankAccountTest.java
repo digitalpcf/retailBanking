e1e8654a84ff91dd8a34e6fec7b92e2eb3f3bfb7
theamistudy <the.amistudy@gmail.com>
2018-11-17 22:06:53 +0530

    Minor change

--- src/test/java/com/digital/bank/account/CucumberBankAccountTest.java
+++ src/test/java/com/digital/bank/account/CucumberBankAccountTest.java
@@ -6,6 +6,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 
+
+
 @RunWith(Cucumber.class)
 @ActiveProfiles("test")
 @CucumberOptions(features = "src/main/resources/feature", glue="com.digital.bank.account.stepdef")
